6ec7f6195fe0ad980a3ad04d5925ab69f3b3c49b
Adrià <128262424+airdac@users.noreply.github.com>
2024-03-25 18:15:53 +0100

    Environment created, data explored, project proposal written
    Python Environment created with basic libraries.
    
    Data fastly explored in a Jupyter Notebook.
    
    Project Proposal written in Google Drive and exported to pdf.

--- package_example.py
+++ package_example.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import seaborn as sns
 from palmerpenguins import load_penguins
+
 sns.set_style('whitegrid')
 
 penguins = load_penguins()
-penguins.head()
+print(penguins.head())
